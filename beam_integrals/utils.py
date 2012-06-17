0f43e90cdd5333aa1e89423488355656edc638f1
Petar Mari? <petar.maric@gmail.com>
2012-06-17 05:07:49 +0200

    Added a mixin for extracting friendly names from classes, based on `django.db.models.options.get_verbose_name`

--- beam_integrals/utils.py
+++ beam_integrals/utils.py
@@ -1,3 +1,19 @@
+import re
+
+
+# Based on `django.db.models.options.get_verbose_name`
+class FriendlyNameFromClassMixin(object):
+    _name_re = re.compile('(((?<=[a-z])[A-Z])|([A-Z](?![A-Z0-9]|$)))')
+    
+    @property
+    def name(self):
+        if not hasattr(self, '_name'):
+            class_name = self.__class__.__name__
+            self._name = self._name_re.sub(r' \1', class_name).strip()
+        
+        return self._name
+
+
 # Based on http://djangosnippets.org/snippets/542/
 class PluginMount(type):
     def __init__(cls, name, bases, attrs): #@UnusedVariable
