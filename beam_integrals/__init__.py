14ff6f9314b8c0d3dd96c4300310a4e2d70cdcae
Petar Mari? <petar.maric@gmail.com>
2012-06-25 01:35:30 +0200

    Added sympy to project requirements, along with some boilerplate code

--- beam_integrals/__init__.py
+++ beam_integrals/__init__.py
@@ -1,7 +1,23 @@
 import os
+from sympy import mpmath, symbols
+
+
+if 'MPMATH_NOGMPY' not in os.environ and mpmath.libmp.BACKEND == 'python': #@UndefinedVariable
+    import warnings
+    from .exceptions import PerformanceWarning
+    
+    warnings.warn(
+        'gmpy is not available for speedups (code will still run correctly, '\
+        'but much slower at high precision)',
+        category=PerformanceWarning
+    )
 
 
 PROJECT_SETTINGS_DIR = os.path.expanduser('~/.beam_integrals')
 DEFAULT_MAX_MODE = 100
 
 __version__ = '0.1.0-pre-alpha'
+
+
+# Commonly used symbols
+a, y, mu_m = symbols('a, y, mu_m')
