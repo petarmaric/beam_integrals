b0ba680f678bdb17382b2b7c380fb1e87057e39b
Petar Marić <petar.maric@gmail.com>
2012-07-26 03:09:59 +0200

    Made project requirements gathering declarative and way more flexible when handling Python version specific requirements

--- setup.py
+++ setup.py
@@ -1,51 +1,15 @@
 from distribute_setup import use_setuptools
 use_setuptools() 
 
-import re
 import sys
 from setuptools import setup, find_packages
+from requirements_utils import parse_dependency_links, parse_requirements
 
 
 if sys.version_info < (2, 6):
     print 'ERROR: beam_integrals requires at least Python 2.6 to run.'
     sys.exit(1)
 
-
-# Thanks to http://cburgmer.posterous.com/pip-requirementstxt-and-setuppy
-def parse_requirements(file_name):
-    requirements = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'(\s*#)|(\s*$)', line):
-            continue
-        if re.match(r'\s*-e\s+', line):
-            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
-        elif re.match(r'\s*-f\s+', line):
-            pass
-        else:
-            requirements.append(line)
-    
-    return requirements
-
-# Thanks to http://cburgmer.posterous.com/pip-requirementstxt-and-setuppy
-def parse_dependency_links(file_name):
-    dependency_links = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'\s*-[ef]\s+', line):
-            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
-    
-    return dependency_links
-
-def get_test_requirements():
-    """
-    Python 2.7 introduced new assert* methods in `unittest.TestCase`, fallback
-    to backported library if Python < 2.7 
-    """
-    requirements = parse_requirements('requirements-test.txt')
-    
-    if sys.version_info < (2, 7):
-        requirements.append('unittest2')
-    
-    return requirements
 
 setup(
     name='beam_integrals',
@@ -76,6 +40,6 @@ setup(
     install_requires = parse_requirements('requirements.txt'),
     dependency_links = parse_dependency_links('requirements.txt'),
     setup_requires = ['nose>=1.0'],
-    tests_require = get_test_requirements(),
+    tests_require = parse_requirements('requirements-test.txt'),
     test_suite = 'nose.collector',
 )
