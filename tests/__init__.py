0a6e1b954416a31a6776ab15ab11ef3f6582badf
Petar Mari? <petar.maric@gmail.com>
2013-01-31 21:49:34 +0100

    Added an option to avoid a well-known memory leak during tests caused by the SymPy internal cache (see goo.gl/B3PhB and goo.gl/pGj10)

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,6 +11,17 @@ MAX_ERROR_TOLERANCE_DECIMALS = 16
 MAX_ERROR_TOLERANCE = 10 ** -MAX_ERROR_TOLERANCE_DECIMALS
 
 
+env = lambda e: os.environ.get(e, '').lower()
+if env('IGNORE_SYMPY_MEMORY_LEAK') != 'yes' and env('SYMPY_USE_CACHE') != 'no':
+    raise RuntimeError(
+        "To avoid a well-known memory leak (see goo.gl/B3PhB and goo.gl/pGj10) "\
+        "you're strongly advised to disable SymPy internal caching, by "\
+        "setting the environment variable 'SYMPY_USE_CACHE=no' before running "\
+        "tests (tests may run a bit slower). To ignore this error set the "\
+        "environment variable 'IGNORE_SYMPY_MEMORY_LEAK=yes'."
+    )
+
+
 # When testing on Windows monkey patch `multiprocessing.Pool()` to use a dummy
 # implementation, to prevent the entire system from blowing up (quite literally,
 # fork bomb). This issue is caused by the lack of a sensible `fork()` syscall on
