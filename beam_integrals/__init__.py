31443d07ad760df72254a000f6322292741efee1
Petar Mari? <petar.maric@gmail.com>
2015-09-03 19:13:08 +0200

    Update packaging info for the 1.0.0 release

--- beam_integrals/__init__.py
+++ beam_integrals/__init__.py
@@ -17,7 +17,7 @@ PROJECT_SETTINGS_DIR = os.path.expanduser('~/.beam_integrals')
 DEFAULT_MAX_MODE = 100
 DEFAULT_DECIMAL_PRECISION = 155
 
-__version__ = '0.1.0-pre-alpha'
+__version__ = '1.0.0'
 
 
 # Commonly used symbols
